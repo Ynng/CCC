d5e667f8bf1574f3afae7a2f88f048042743db98
Ynng <44907675+Ynng@users.noreply.github.com>
2019-01-20 14:48:55 -0500

    finished 14c2p4 for DOMPC

--- DMOPC/14c2p4.cpp
+++ DMOPC/14c2p4.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-
-int main() {
-	return 0;
+int main()
+{
+    int n, q, temp, treeTotalMass = 0;
+    vector<int> trees, queryA, queryB;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        treeTotalMass += temp;
+        trees.push_back(treeTotalMass);
+    }
+    scanf("%d", &q);
+    for (int i = 0; i < q; i++)
+    {
+        scanf("%d ", &temp);
+        queryA.push_back(temp);
+        scanf("%d", &temp);
+        queryB.push_back(temp);
+    }
+    for (int i = 0; i < q; i++)
+    {
+        if (queryA[i] == 0)
+        {
+            printf("%d\n", trees[queryB[i]]);
+        }
+        else
+        {
+            printf("%d\n", trees[queryB[i]] - trees[queryA[i] - 1]);
+        }
+    }
+    return 0;
 }
