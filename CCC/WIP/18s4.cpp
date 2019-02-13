f700350727c033bc8109869c7d0473665255cd16
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-02-13 17:11:57 -0500

    improved 18s4 a bit

--- CCC/WIP/18s4.cpp
+++ CCC/WIP/18s4.cpp
@@ -1,34 +1,62 @@
-#include "pch.h"
 #include <stdio.h>
-#include <math.h>  
 #include <map>
+#include <cmath>
 using namespace std;
-int n = 0;
-std::map<int, double> theMap;
-double node(int weight) {
+int n;
+long long complexity = 0;
+map<int, long long> dp;
+
+long long node(int weight)
+{
 	int nodeWeight;
-	double d = 0, temp = 0;
-	for (int i = 2; i <= weight; i++) {
+	long long d = 0, temp = 0;
+	//i = number of nodes
+	for (int i = 2; i <= weight / 3; i++)
+	{
 		//nodeWeight = (weight - (weight%i)) / i;
-		nodeWeight = floor(weight / i);
-		if (theMap.count(nodeWeight)) {
-			d += theMap[nodeWeight];
+		nodeWeight = weight / i;
+		if (dp.count(nodeWeight))
+		{
+			d += dp[nodeWeight];
 		}
-		else {
+		else
+		{
 			temp = node(nodeWeight);
-			theMap[nodeWeight]=temp;
+			dp[nodeWeight] = temp;
 			d += temp;
 		}
+		complexity++;
 	}
+	d += (weight - weight / 3);
+
 	return d;
 }
 
-int main() {
+int main()
+{
 	scanf("%d", &n);
-	if (n == 1) {
+	if (n == 1 || n == 2)
+	{
 		printf("%d", 1);
 		return 0;
 	}
-	printf("%d", node(n));
+	// dp[1] = 1;
+	// dp[2] = 1;
+	// for (int i = 3; i <= n; i++)
+	// {
+	// 	dp[i] = 0;
+	// 	for (int j = 2; j <= i / 2; j++)
+	// 	{
+	// 		dp[i] += dp[i / j];
+	// 	}
+	// 	dp[i]+=i/2+i%2;
+	// }
+	printf("%lld", node(n));
+	// for (int i = 0; i <= n; i++)
+	// {
+	// 	if (dp[i] != 0)
+	// 		printf("\n%d %lld", i, dp[i]);
+	// }
+	printf("\nComplexity %lld", complexity);
 	return 0;
 }
