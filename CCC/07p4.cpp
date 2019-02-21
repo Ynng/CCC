991af7fa7f5cc9d4d394b2a59fc0c136d4dcface
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-02-21 15:05:49 -0500

    tried to do 07p4 (not even close)

---
+++ CCC/07p4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, p, ridings[100005][15];
+    scanf("%d", &n);
+    scanf("%d", &p);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= p; j++)
+        {
+            scanf("%d", &ridings[i][j]);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+            
+    }
+}
