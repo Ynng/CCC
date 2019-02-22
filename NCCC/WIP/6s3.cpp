3f98f933e0331273618cd648dac7dd6fdd76a0b2
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-02-22 14:41:53 -0500

    tried to redo nccc6s3 (kinda

--- NCCC/WIP/6s3.cpp
+++ NCCC/WIP/6s3.cpp
@@ -12,46 +12,6 @@ int main()
         winds[tempX][tempY] = true;
     }
 
-    // for (int i = 0; i < x; i++)
-    // {
-    //     for (int j = 0; j < y; j++)
-    //     {
-    //         winds[i][j] = false;
-    //     }
-    // }
-    time = step(0, xs, ys, 0, 0);
     cout << time << endl;
     return 0;
-}
-
-int step(int time, int x, int y, int vx, int vy)
-{
-    //checking for the wind
-    if (vx > 0)
-    {
-        for (int i = x; i < x + vx; i++)
-        {
-            if(it)
-        }
-    }
-    else if (vx < 0)
-    {
-        for (int i = x; i > x - vx; i--)
-        {
-        }
-    }
-    if (vx > 0)
-    {
-        for (int i = x; i < x + vx; i++)
-        {
-        }
-    }
-    else if (vx < 0)
-    {
-        for (int i = x; i > x - vx; i--)
-        {
-        }
-    }
-
-    step()
 }
