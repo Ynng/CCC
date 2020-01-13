0a6eeee8b35f58b0f87c0e1575590f3b57f6986c
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-12 23:53:07 -0500

    struggles on 19s4

--- CCC/WIP/19s4.cpp
+++ CCC/WIP/19s4.cpp
@@ -1,30 +1,73 @@
-
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
+
+int n, k;
+int minimumDays, slack;
+long long output;
+vector<long long> attraction;
+
+int largest(int start, int end)
+{
+    // printf("\nlargest on %d %d", start, end);
+
+    long long maximum = 0;
+    for (int i = start; i < end && i < attraction.size(); i++)
+    {
+        if (attraction[i] > maximum)
+            maximum = attraction[i];
+    }
+    return maximum;
+}
+
+int recursion(int startDate, int slackRemaining)
+{
+    // printf("\nrecursion on %d %d", startDate, slackRemaining);
+    long long temp, maximum = 0;
+
+    if (startDate - slackRemaining + k < n)
+    {
+        for (int i = 0; i <= slackRemaining; i++)
+        {
+            temp = recursion(startDate + k - i, slackRemaining - i);
+            temp += largest(startDate, startDate + k - i);
+            if (temp > maximum)
+                maximum = temp;
+        }
+    }
+    else
+    {
+        temp = largest(startDate, startDate + k);
+        if (temp > maximum)
+            maximum = temp;
+    }
+    return maximum;
+}
+
 int main()
 {
-    int n, k, temp, output;
+    long long temp;
     output = -1;
-    vector<long long> attraction;
     scanf("%d %d", &n, &k);
+    minimumDays = ceil(n / k);
+    slack = k - n % k;
+    if (slack == k)
+        slack = 0;
+
     for (int i = 0; i < n; i++)
     {
         scanf("%lld", &temp);
         attraction.push_back(temp);
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= ; j++)
-        {
-            temp = 
-        }
-    }
+    output = recursion(0, slack);
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d", attraction[i]);
-    }
+    printf("%lld", output);
+
+    //     for (int i = 0; i < n; i++)
+    // {
+    //     printf("%d", attraction[i]);
+    // }
     return 0;
 }
