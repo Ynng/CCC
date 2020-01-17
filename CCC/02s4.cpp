7e9181c0d991864436ccc957cff3dbe3d6b45ea1
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-17 17:28:27 -0500

    tested changing indents

--- CCC/02s4.cpp
+++ CCC/02s4.cpp
@@ -8,70 +8,72 @@ string names[105];
 
 int slowest(int n, int m)
 {
-    int temp = 0;
-    for (int i = n; i <= m; i++)
-    {
-        temp = fmax(temp, times[i]);
-    }
-    return temp;
+  int temp = 0;
+  for (int i = n; i <= m; i++)
+  {
+    temp = fmax(temp, times[i]);
+  }
+  return temp;
 }
 
 int main()
 {
-    dp[0][0] = 0;
-    dp[0][1] = 0;
-    times[0] = 0;
+  dp[0][0] = 0;
+  dp[0][1] = 0;
+  times[0] = 0;
 
-    scanf("%d", &m);
-    scanf("%d", &q);
+  scanf("%d", &m);
+  scanf("%d", &q);
 
-    for (int i = 1; i <= q; i++)
+  for (int i = 1; i <= q; i++)
+  {
+    cin >> names[i];
+    // scanf("%s", &names[i]);
+    scanf("%d", &times[i]);
+  }
+
+  for (int i = 1; i <= q; i++)
+  {
+    tempA = INT_MAX;
+    tempB = 0;
+    for (int j = 1; j <= m; j++)
     {
-        cin >> names[i];
-        // scanf("%s", &names[i]);
-        scanf("%d", &times[i]);
-    }
-
-    for (int i = 1; i <= q; i++)
-    {
-        tempA = INT_MAX;
-        tempB = 0;
-        for (int j = 1; j <= m; j++)
+      if (i - j >= 0)
+      {
+        if (dp[i - j][0] + slowest(i - j + 1, i) < tempA)
         {
-            if (i - j >= 0)
-            {
-                if (dp[i - j][0] + slowest(i - j + 1, i) < tempA)
-                {
-                    tempA = dp[i - j][0] + slowest(i - j + 1, i);
-                    tempB = i - j;
-                }
-            }
+          tempA = dp[i - j][0] + slowest(i - j + 1, i);
+          tempB = i - j;
         }
-        dp[i][0] = tempA;
-        dp[i][1] = tempB;
+      }
     }
+    dp[i][0] = tempA;
+    dp[i][1] = tempB;
+  }
 
-    // printf("\n\n\n");
-    // for (int i = 1; i <= q; i++)
-    // {
-    //     printf("Total Time: %d\n", dp[i][0]);
-    // }
+  // printf("\n\n\n");
+  // for (int i = 1; i <= q; i++)
+  // {
+  //     printf("Total Time: %d\n", dp[i][0]);
+  // }
 
-    printf("Total Time: %d\n", dp[q][0]);
+  printf("Total Time: %d\n", dp[q][0]);
 
-    tempA = q;
-    while (tempA > 0)
-    {
-        tempA = dp[tempA][1];
-        outputs[tempA] = true;
-    }
-    for (int i = 1; i <= q; i++)
-    {
-        // printf("%s", names[i]);
-        cout<<names[i];
-        if(outputs[i])printf("\n");
-        else printf(" ");
-    }
+  tempA = q;
+  while (tempA > 0)
+  {
+    tempA = dp[tempA][1];
+    outputs[tempA] = true;
+  }
+  for (int i = 1; i <= q; i++)
+  {
+    // printf("%s", names[i]);
+    cout << names[i];
+    if (outputs[i])
+      printf("\n");
+    else
+      printf(" ");
+  }
 
-    return 0;
+  return 0;
 }
--- CCC/14s3.cpp
+++ CCC/14s3.cpp
@@ -2,90 +2,90 @@
 #include <vector>
 int main()
 {
-    std::vector<int> cars;
-    std::vector<int> side;
-    int t, n, temp, counter, cur;
-    bool tempOutput;
-    bool output[100] = {false};
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++)
+  std::vector<int> cars;
+  std::vector<int> side;
+  int t, n, temp, counter, cur;
+  bool tempOutput;
+  bool output[100] = {false};
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++)
+  {
+    cars.clear();
+    side.clear();
+    scanf("%d", &n);
+    for (int j = 1; j <= n; j++)
     {
-        cars.clear();
-        side.clear();
-        scanf("%d", &n);
-        for (int j = 1; j <= n; j++)
-        {
 
-            scanf("%d", &temp);
-            cars.push_back(temp);
-        }
-        counter = 1;
-        tempOutput = true;
-        while (cars.size() > 0 || side.size() > 0)
-        {
-            if (cars.size() > 0)
-            {
-                cur = cars.back();
-                if (cur == counter)
-                {
-                    cars.pop_back();
-                    counter++;
-                }
-                else if (cur < counter)
-                {
-                    tempOutput = false;
-                    break;
-                }
-                else if (cur > counter)
-                {
-                    if (side.size() > 0 && side.back() == counter)
-                    {
-                        side.pop_back();
-                        counter++;
-                    }
-                    else
-                    {
-                        cars.pop_back();
-                        side.push_back(cur);
-                    }
-                }
-            }
-            else
-            {
-                cur = side.back();
-                side.pop_back();
-                if (cur == counter)
-                {
-                    counter++;
-                }
-                else
-                {
-                    tempOutput = false;
-                    break;
-                }
-            }
-        }
-        output[i] = tempOutput;
+      scanf("%d", &temp);
+      cars.push_back(temp);
     }
-
-    for (int i = 0; i < t - 1; i++)
+    counter = 1;
+    tempOutput = true;
+    while (cars.size() > 0 || side.size() > 0)
     {
-        if (output[i])
+      if (cars.size() > 0)
+      {
+        cur = cars.back();
+        if (cur == counter)
         {
-            printf("Y\n");
+          cars.pop_back();
+          counter++;
+        }
+        else if (cur < counter)
+        {
+          tempOutput = false;
+          break;
+        }
+        else if (cur > counter)
+        {
+          if (side.size() > 0 && side.back() == counter)
+          {
+            side.pop_back();
+            counter++;
+          }
+          else
+          {
+            cars.pop_back();
+            side.push_back(cur);
+          }
+        }
+      }
+      else
+      {
+        cur = side.back();
+        side.pop_back();
+        if (cur == counter)
+        {
+          counter++;
         }
         else
         {
-            printf("N\n");
+          tempOutput = false;
+          break;
         }
+      }
     }
-    if (output[t - 1])
+    output[i] = tempOutput;
+  }
+
+  for (int i = 0; i < t - 1; i++)
+  {
+    if (output[i])
     {
-        printf("Y");
+      printf("Y\n");
     }
     else
     {
-        printf("N");
+      printf("N\n");
     }
-    return 0;
+  }
+  if (output[t - 1])
+  {
+    printf("Y");
+  }
+  else
+  {
+    printf("N");
+  }
+  return 0;
 }
