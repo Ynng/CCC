58251c72b009359f9c365f5b29c3cef57b69cf9a
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-22 15:05:31 -0500

    improved tasks.json, tested it with test.cpp

--- CCC/test.cpp
+++ CCC/test.cpp
@@ -1,15 +1,13 @@
 #include <map>
 #include <iostream>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 
-int main(){
-    map<int,int> mp;
-    mp[1]=0;
-    mp[12]=123;
-    mp[23]=345;
-    for(auto item = mp.begin();item!=mp.end();item++){
-        cout<<item->first<<","<<item->second<<endl;
-    }
-    return 0;
+int main()
+{
+    char s[9];
+    strcpy(s, "too large");
+    cout << s << endl;
 }
