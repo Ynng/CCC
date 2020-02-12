2edb2632abd71f5370bd7605bad45930d2d31771
Ynng <44907675+Ynng@users.noreply.github.com>
2020-02-12 13:42:24 -0500

    Revert "fk it doesnt work"
    This reverts commit 1911f86682ceac9f98ffd7db5436bb84c8801f49.

--- CCC/20s3.cpp
+++ CCC/20s3.cpp
@@ -2,19 +2,13 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
-char n[400005];
-char h[400005];
+char n[200005];
+char h[200005];
 int nLen, hLen;
 long long output;
-map<string, int> dp;
-bool continueIt, breakIt;
-bool letters[50];
-int table[125];
-int a,b,shift;
 int main()
 {
   scanf("%s", n);
@@ -23,78 +17,12 @@ int main()
   hLen = strlen(h);
   output = 0;
 
-  for (int i = 0; i < hLen; i++)
-  {
-    if (letters[h[i]] == false)
-      letters[h[i]] = true;
-  }
-  for (int i = 0; i < nLen; i++)
-  {
-    if (letters[n[i]] == false)
-    {
-      printf("0");
-      return 0;
-    }
-  }
-
   sort(n, n + nLen);
-
   do
   {
     // printf("%s\n", n);
-    continueIt = false;
-    for (int i = 1; i <= nLen; i++)
-    {
-      // printf("%s\n",string(n,i).c_str());
-      if (dp[string(n, i)] == -1)
-      {
-        continueIt = true;
-        break;
-      }
-      else if (dp[string(n, i)] == 0)
-      {
-        dp[string(n, i)] = strstr(h, string(n, i).c_str()) ? 1 : -1;
-      }
-    }
-    if (continueIt)
-      continue;
-
-    for (int i = 0; i < 125; i++)
-    {
-      table[i] = nLen;
-    }
-    for (int i = 0; i < nLen - 1; i++)
-    {
-      table[n[i]] = nLen - i - 1;
-    }
-    shift = table[n[nLen - 1]];
-    table[n[nLen - 1]] = 0;
-    memset(h + hLen, n[nLen - 1], nLen);
-
-
-    a = 0;
-    while (a < hLen)
-    {
-      b = table[h[a + nLen - 1]];
-      while (b != 0)
-      {
-        a += b;
-        b = table[h[a + nLen - 1]];
-        a += b;
-        b = table[h[a + nLen - 1]];
-        a += b;
-        b = table[h[a + nLen - 1]];
-        a += b;
-        b = table[h[a + nLen - 1]];
-        a += b;
-        b = table[h[a + nLen - 1]];
-      }
-      if (memcmp(n, h + a, nLen - 1) == 0 && a < hLen){
-        output++;
-        break;
-      }
-      a += shift;
-    }
+    if (strstr(h, n))
+      output++;
   } while (next_permutation(n, n + nLen));
 
   printf("%lld", output);
