1911f86682ceac9f98ffd7db5436bb84c8801f49
Ynng <44907675+Ynng@users.noreply.github.com>
2020-02-12 13:41:44 -0500

    fk it doesnt work

--- CCC/20s3.cpp
+++ CCC/20s3.cpp
@@ -2,13 +2,19 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
-char n[200005];
-char h[200005];
+char n[400005];
+char h[400005];
 int nLen, hLen;
 long long output;
+map<string, int> dp;
+bool continueIt, breakIt;
+bool letters[50];
+int table[125];
+int a,b,shift;
 int main()
 {
   scanf("%s", n);
@@ -17,12 +23,78 @@ int main()
   hLen = strlen(h);
   output = 0;
 
+  for (int i = 0; i < hLen; i++)
+  {
+    if (letters[h[i]] == false)
+      letters[h[i]] = true;
+  }
+  for (int i = 0; i < nLen; i++)
+  {
+    if (letters[n[i]] == false)
+    {
+      printf("0");
+      return 0;
+    }
+  }
+
   sort(n, n + nLen);
+
   do
   {
     // printf("%s\n", n);
-    if (strstr(h, n))
-      output++;
+    continueIt = false;
+    for (int i = 1; i <= nLen; i++)
+    {
+      // printf("%s\n",string(n,i).c_str());
+      if (dp[string(n, i)] == -1)
+      {
+        continueIt = true;
+        break;
+      }
+      else if (dp[string(n, i)] == 0)
+      {
+        dp[string(n, i)] = strstr(h, string(n, i).c_str()) ? 1 : -1;
+      }
+    }
+    if (continueIt)
+      continue;
+
+    for (int i = 0; i < 125; i++)
+    {
+      table[i] = nLen;
+    }
+    for (int i = 0; i < nLen - 1; i++)
+    {
+      table[n[i]] = nLen - i - 1;
+    }
+    shift = table[n[nLen - 1]];
+    table[n[nLen - 1]] = 0;
+    memset(h + hLen, n[nLen - 1], nLen);
+
+
+    a = 0;
+    while (a < hLen)
+    {
+      b = table[h[a + nLen - 1]];
+      while (b != 0)
+      {
+        a += b;
+        b = table[h[a + nLen - 1]];
+        a += b;
+        b = table[h[a + nLen - 1]];
+        a += b;
+        b = table[h[a + nLen - 1]];
+        a += b;
+        b = table[h[a + nLen - 1]];
+        a += b;
+        b = table[h[a + nLen - 1]];
+      }
+      if (memcmp(n, h + a, nLen - 1) == 0 && a < hLen){
+        output++;
+        break;
+      }
+      a += shift;
+    }
   } while (next_permutation(n, n + nLen));
 
   printf("%lld", output);
