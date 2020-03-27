9091a501b3acf8017d9fcf06333a857b8d6c7085
Ynng <44907675+Ynng@users.noreply.github.com>
2020-03-26 23:48:55 -0400

    added double hashing, finally it works

--- CCC/20s3.cpp
+++ CCC/20s3.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 #include <algorithm>
 #include <map>
-#include <unordered_set>
+#include <set>
 #include <math.h>
 // #define DEBUG
 
@@ -15,12 +15,13 @@ char h[200005];
 int nLen, hLen;
 ll output;
 ll hsh[200005];
+ll hsh2[200005];
 int charCount[200];
 int needleCharCount[200];
-int base = 131;
-ll diff;
+int base = 131, base2 = 7919;
+ll diff, diff2;
 ll mod = 1e9 + 7;
-unordered_set<ll> ans;
+set<pair<ll,ll>> ans;
 
 bool checkSubstr()
 {
@@ -35,6 +36,11 @@ bool checkSubstr()
 ll getHash(int i)
 {
   return (hsh[i]% mod - (hsh[i - nLen] * diff) % mod +mod)%mod;
+}
+
+ll getHash2(int i)
+{
+  return (hsh2[i]% mod - (hsh2[i - nLen] * diff2) % mod +mod)%mod;
 }
 
 long long powll(long long base, long long exp)
@@ -55,6 +61,7 @@ int main()
   output = 0;
 
   diff = powll(base, nLen);
+  diff2 = powll(base2, nLen);
 
 #ifdef DEBUG
   printf("diff is %lld", diff);
@@ -70,10 +77,11 @@ int main()
     needleCharCount[n[i]]++;
     charCount[h[i]]++;
   }
-  hsh[0] = h[0] - 'a' + 1;
+  hsh2[0] = hsh[0] = h[0] - 'a' + 1;
   for (int i = 1; i < hLen; i++)
   {
     hsh[i] = ((hsh[i - 1] * base + h[i] - 'a' + 1) % mod + mod) % mod;
+    hsh2[i] = ((hsh2[i - 1] * base2 + h[i] - 'a' + 1) % mod + mod) % mod;
     #ifdef DEBUG
     printf("\nhash[%d] is %lld", i, hsh[i]);
     #endif
@@ -81,7 +89,7 @@ int main()
 
   if (checkSubstr())
   {
-    ans.insert(hsh[nLen - 1]);
+    ans.insert({hsh[nLen - 1], hsh2[nLen - 1]});
     #ifdef DEBUG
     printf("\nadded %lld to ans", hsh[nLen - 1]);
     #endif
@@ -97,7 +105,7 @@ int main()
 
     if (checkSubstr())
     {
-      ans.insert(getHash(i));
+      ans.insert({getHash(i), getHash2(i)});
       #ifdef DEBUG
       printf("\nadded %lld to ans, i = %d", getHash(i), i);
       #endif
