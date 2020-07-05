4644466d7c8a717f5b62f613f6b99b244b6aa920
Ynng <44907675+Ynng@users.noreply.github.com>
2020-07-04 22:13:37 -0400

    simplified answer

--- OCC/occ19b3.cpp
+++ OCC/occ19b3.cpp
@@ -15,13 +15,9 @@ int main(int argc, const char **argv)
 
   int n;
   scanf("%d", &n);
-  if(n % 4 == 0){
+  if (n % 4 == 0 || n % 4 == 3)
     printf("Yes");
-  }else if(n % 2 != 0 && ((n + 1) / 2) % 2 == 0){
-    printf("Yes");
-  }else
-  {
+  else
     printf("No");
-  }
   return 0;
 }
