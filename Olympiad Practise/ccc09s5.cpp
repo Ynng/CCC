ab0e2e8baee838c5177d14288107d5b3d6f47dee
Ynng <44907675+Ynng@users.noreply.github.com>
2020-07-07 16:20:57 -0400

    Finished CCC 09s5

--- Olympiad Practise/ccc09s5.cpp
+++ Olympiad Practise/ccc09s5.cpp
@@ -14,19 +14,19 @@
 #include <climits>
 #define f first
 #define s second
-using namespace std; 
+using namespace std;
 typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
- 
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
 typedef vector<int> vi;
 typedef vector<ld> vd;
 typedef vector<ll> vl;
 typedef vector<pi> vpi;
 typedef vector<pl> vpl;
- 
+
 #define sz(x) (int)(x).size()
 #define mp make_pair
 #define pb push_back
@@ -38,12 +38,43 @@ typedef vector<pl> vpl;
 #define ins insert
 
 #define MOD 1000000007
-#define MAXN 10005
 
-int variables;
-int main(){
-    
-    
-    
+int M, N, K;
+int a[30005][1005];
+int main()
+{
+    scanf("%d %d %d", &M, &N, &K);
+    for (int i = 0, x, y, r, b; i < K; i++)
+    {
+        scanf("%d %d %d %d", &x, &y, &r, &b);
+        for (int j = max(1, x - r), t, upper, lower; j <= min(N, x + r); j++)
+        {
+            t = j - x;
+            upper = sqrt(r * r - t * t);
+            lower = -1 - upper;
+            upper = min(M, y + upper);
+            lower = max(0, y + lower);
+            a[upper][j] += b;
+            a[lower][j] -= b;
+        }
+    }
+
+    int maxb = 0, maxOccur = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = M; j >= 1; j--)
+        {
+            a[j][i] += a[j + 1][i];
+            if (a[j][i] > maxb)
+            {
+                maxOccur = 1;
+                maxb = a[j][i];
+            }
+            else if (a[j][i] == maxb)
+                maxOccur++;
+        }
+    }
+
+    printf("%d\n%d", maxb, maxOccur);
     return 0;
 }
