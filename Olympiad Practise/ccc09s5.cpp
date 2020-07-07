93957b5e4db920c2ac07ac2c73aef6dc936af68d
Ynng <44907675+Ynng@users.noreply.github.com>
2020-07-07 00:37:36 -0400

    new template

--- Olympiad Practise/ccc09s5.cpp
+++ Olympiad Practise/ccc09s5.cpp
@@ -1,20 +1,49 @@
-#include <iostream>
-#include <string>
+#include <stdio.h>
 #include <cstring>
+#include <iostream>
 #include <algorithm>
+#include <vector>
+#include <math.h>
 #include <map>
 #include <set>
-#include <math.h>
-using namespace std;
-
+#include <unordered_set>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <climits>
+#define f first
+#define s second
+using namespace std; 
 typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
 
 #define MOD 1000000007
 #define MAXN 10005
-// #define DEBUG
 
+int variables;
 int main(){
-  //code goes here
-  
-  return 0;
+    
+    
+    
+    return 0;
 }
