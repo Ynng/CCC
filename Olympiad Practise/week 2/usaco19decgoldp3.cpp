f2aee51cf934e0f26ab97ba5050614d0246607a5
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-16 19:14:43 -0400

    WIP

---
+++ Olympiad Practise/week 2/usaco19decgoldp3.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <climits>
+#define f first
+#define s second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+#define MOD 1000000007
+#define MAXN 10005
+
+int N, M, K;
+int main()
+{
+  scanf("%d %d %d", &N, &M, &K);
+
+  //Dijkstra to find 
+  return 0;
+}
