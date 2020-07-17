3f197f2d66c84d2852c410e337ef6c0d8ab8cc95
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-17 17:04:08 -0400

    wip

---
+++ Olympiad Practise/other/coci18c6p5.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <climits>
+#define f first
+#define s second
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+#define MOD 1000000007
+#define MAXN 10005
+
+int R, S, N;
+int a[305][305];
+multiset<int> s[305][305];
+
+#define C s[i][j]
+#define CV a[i][j]
+int main()
+{
+  scanf("%d %d %d", &R, &S, &N);
+  for (int i = 1; i <= R; i++)
+  {
+    for (int j = 1; j <= S; j++)
+    {
+      scanf("%d", a[i] + j);
+
+      if(i==1 && j == 1)
+      C.insert(CV);
+      C.find()
+    }
+  }
+
+
+
+  return 0;
+}
