b4c41aee7e32ed912be74f2332bf005f23b888dc
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-18 15:55:52 -0400

    finished usaco 2019 dec gold p3 with class

--- Olympiad Practise/week 2/usaco19decgoldp3.cpp
+++ Olympiad Practise/week 2/usaco19decgoldp3.cpp
@@ -38,13 +38,47 @@ typedef vector<pl> vpl;
 #define ins insert
 
 #define MOD 1000000007
-#define MAXN 10005
+#define MAXN 100005
 
-int N, M, K;
+int N, M, K, dp[MAXN], psa[MAXN][30], best[MAXN][30];
+char c[MAXN];
+int dis[30][30];
 int main()
 {
   scanf("%d %d %d", &N, &M, &K);
 
-  //Dijkstra to find 
+  for (int i = 1; i <= N; i++)
+    scanf(" %c", c + i);
+
+  for (int i = 0; i < M; i++)
+    for (int j = 0; j < M; j++)
+      scanf(" %d", dis[i] + j);
+
+  //Dijkstra on a loop to find?
+
+  //floyd warshall
+  for (int k = 0; k < M; k++)
+    //middle "transfer" node
+    for (int i = 0; i < M; i++)
+      for (int j = 0; j < M; j++)
+        if (dis[i][j] > dis[i][k] + dis[k][j])
+          dis[i][j] = dis[i][k] + dis[k][j];
+
+  for (int i = 1; i <= N; i++)
+    for (int j = 0; j < M; j++)
+      psa[i][j] = dis[c[i] - 'a'][j] + psa[i - 1][j];
+
+  memset(dp, 0x3f, sizeof(dp));
+  dp[0] = 0;
+  for (int i = K; i <= N; i++)
+  {
+    for (int p = 0; p < M; p++)
+      //think about this part some more
+      dp[i] = min(dp[i], best[i - K][p] + psa[i][p]);
+    for (int p = 0; p < M; p++)
+      best[i][p] = min(best[i - 1][p], dp[i] - psa[i][p]);
+  }
+
+  printf("%d", dp[N]);
   return 0;
 }
