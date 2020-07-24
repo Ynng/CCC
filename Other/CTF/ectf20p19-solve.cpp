eb01ab1a544a79c6734dd2516a28427e1f1d20e8
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-24 17:44:18 -0400

    I give up ectf20p19

---
+++ Other/CTF/ectf20p19-solve.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <bitset>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <climits>
+using namespace std; 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<ll, int> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7, MX = 10005;
+
+int variables;
+int main()
+{
+  for (int i = 0; i < 150; i++)
+  {
+    printf("A");
+  }
+  scanf("%d");
+  
+  return 0;
+}
