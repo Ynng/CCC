ff4c21de912587718ab7a67d31886615e990dd05
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-07-24 16:58:02 -0400

    did  ectf20p18, overthinked it way too much

--- Other/CTF/ectf20p18-main.c
+++ Other/CTF/ectf20p18-main.c
@@ -7,16 +7,25 @@ main()
 {
     fprintf(stderr, "Welcome to echo, live edition!\n");
     fprintf(stderr, "Enter something and this program will repeat it.\n");
-    char something_secret[64];
-    FILE *f = fopen("flag", "r");
-    if (f == NULL)
-      {
-        fprintf(stderr, "Failed to open flag file.\n");
-        return 1;
-      }
-    fgets(something_secret, 64, f);
+    char something_secret[64] = "Testing test";
+    // FILE *f = fopen("flag", "r");
+    // if (f == NULL)
+    //   {
+    //     fprintf(stderr, "Failed to open flag file.\n");
+    //     return 1;
+    //   }
+    // fgets(something_secret, 64, f);
+
     char data[64] = {0};
+    ll
+    printf("flag address: %d\n", something_secret);
+    printf("data address: %d\n", data);
+    
     read(0, data, 63);
+
+    // printf("Testing: %c%c%c%c%c%c\n", data[64],data[65],data[66],data[67],data[68],data[69]);
+
+    // printf("Testing: %c%c%c%c%c%c\n", data[0],data[1],data[2],data[3],data[4],data[5]);
     fprintf(stderr, data);
 
     scanf("%d");
