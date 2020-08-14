6b4c8f7cd9c1ca8192f4b81f533e104a2a846b48
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-08-14 15:27:28 -0400

    I hate edge cases and implementation and string algo

--- Olympiad Practise/week 6/acc3p3.cpp
+++ Olympiad Practise/week 6/acc3p3.cpp
@@ -41,9 +41,67 @@ typedef vector<pl> vpl;
 const int MOD = 1e9+7, MX = 100000 + 5;
 
 int N;
+string str[MX];
+int cutOffPos[MX];
+string answer = "";
 int main()
 {
-  
+  scanf("%d", &N);
+  for(int i = 1; i <= N; i++)
+  {
+    cin >> str[i];
+  }
+
+  for (char i = 'z'; i >= 'a'; i--)
+  {
+    bool existInAll = true;
+    int minLength = 10000000;
+    for(int j = 1; j <= N; j++)
+    {
+      bool found = false;
+      int foundCount = 0;
+      int strlen = str[j].length();
+      for (int k = 0; k < strlen; k++)
+      {
+        if(str[j].at(k) == i){
+          if(!found)
+            cutOffPos[j] = k;
+          foundCount++;
+          found = true;
+        }else if(found){
+          break;
+        }
+      }
+      if(found){
+        minLength = min(minLength, foundCount);
+      }
+
+      if(!found){
+        existInAll = false;
+        break;
+      }
+    }
+    if(existInAll){
+      for(int j = 1; j <= N; j++){
+        int cutoffIndex = cutOffPos[j]+minLength;
+        if(cutoffIndex >= str[j].length())
+          str[j] = "";
+        else
+          str[j] = str[j].substr(cutoffIndex);
+      }
+      while(minLength > 0){
+        answer += i;
+        minLength--;
+      }
+      i++;
+    }
+  }
+
+  if(answer.length() > 0){
+    cout << answer << endl;
+  }else{
+    printf("-1\n");
+  }
   
   
   return 0;
