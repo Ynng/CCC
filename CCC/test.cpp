87c533f075ed5c46b63567a9bcc58e84c257226e
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-02-20 22:57:08 -0500

    helped peter

--- CCC/test.cpp
+++ CCC/test.cpp
@@ -30,59 +30,10 @@ typedef vector<pl> vpl;
 
 const int MOD = 1000000007, MX = 10000 + 5;
 
-pi bit[MX];
-int subway[MX];
-int subwayR[MX];
-
-pi queryMin(int r)
-{
-  pi mi = {MX+10, 0};
-  while (r > 0)
-  {
-    mi=min(bit[r],mi);
-    r ^= (r & -r);
-  }
-  return mi;
-}
-
-void update(int val, int idx)
-{
-  int oriIdx = idx;
-  while (idx <= MX-4)
-  {
-    if(bit[idx].s == oriIdx){
-      if(val > bit[idx].f){
-        //find replacement
-        int repIdx = idx - (idx & -idx)/2;
-        bit[idx] = {val,oriIdx};
-        pi rep = bit[repIdx];
-        if(rep.f < val)bit[idx]=rep;
-      }else{
-        bit[idx] = {val,oriIdx};
-      }
-    }else{
-      if(val < bit[idx].f || bit[idx].f==0){
-        bit[idx] = {val,oriIdx};
-      }
-    }
-    idx += (idx & -idx);
-  }
-}
-
+int variables;
 int main()
 {
-  while(true){
-    int val, idx;
-    scanf("%d %d", &val, &idx);
-    update(val, idx);
-    printf("\n");
-    for (int i = 1; i < 10; i++)
-    {
-      printf("%d ", bit[i]);
-    }
-    printf("\n");
-  }
-  
-  
   return 0;
+  
+  printf("Hello World");
 }
