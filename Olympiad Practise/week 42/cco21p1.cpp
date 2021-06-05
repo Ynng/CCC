0650425431b720fb727f9611094a02ff97cf6fb7
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-06-05 16:26:20 -0400

    test

---
+++ Olympiad Practise/week 42/cco21p1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define INF 0x3f3f3f3f            // for int
+#define LL_INF 0x3f3f3f3f3f3f3f3f // for ll
+#define sz(x) (int)(x).size()
+#define ms(x, y) memset(x, y, sizeof(x))
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1000000007, MX = 1000000 + 5;
+
+int N, K, Q, a[MX];
+int main()
+{
+  scanf("%d %d %d", &N, &K, &Q);
+  for(int i = 1; i <= N; i++)
+  {
+    scanf("%d", a+i);
+  }
+  for(int i = 1; i <= Q; i++)
+  {
+    
+  }
+  
+  
+  return 0;
+}
